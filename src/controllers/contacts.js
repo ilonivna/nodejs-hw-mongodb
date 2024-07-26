727cb9e76f0daf86e0f9ce118ede41f3f4585197
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-07-26 12:54:36 +0300

    filter by all criteria

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,6 +7,8 @@ export const getContactsController = async (req, res, next) => {
     try {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
             data: contacts,
         });
     } catch (err) {
